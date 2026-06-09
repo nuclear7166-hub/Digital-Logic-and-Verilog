3dfed1fa14bc3756337b76ee85e4df3bbe101db4
hyeonwoo <nuclear7166@gmail.com>
2026-06-09 17:24:52 +0900

    Add files via upload

---
+++ 4-bit Shift Register/shift4.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module shift4(
+    input RESETN, DATA_IN, CLK,
+    output reg [3:0] Q
+    );
+    
+    always @(posedge RESETN or posedge CLK)     begin
+    if(RESETN == 1'b1)
+    Q <= 4'b0000;
+    else        begin
+    Q[2:0] <= Q[3:1];
+    Q[3] <= DATA_IN;    end
+    end
+endmodule
